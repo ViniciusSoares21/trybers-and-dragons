83a7266a85eb51d427fd8076295d819d0a559757
Vinicius Soares <viniciussoares72943@gmail.com>
2023-01-19 07:41:29 -0300

    cria class abstrata Race para criar raças

---
+++ src/Races/Race.ts
@@ -0,0 +1,22 @@
+export default abstract class Race {
+  constructor(
+    private readonly _name:string, 
+    private readonly _dexterity:number,
+  ) {}
+
+  get name():string {
+    return this._name;
+  }
+
+  get dexterity():number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(value:unknown):number | void {
+    if (!value) {
+      throw new Error('Not implemented');
+    }
+  }
+
+  abstract get maxLifePoints():number;
+}
